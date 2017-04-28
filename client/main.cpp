628ece031a0b709f9eb1a7b99c13af36260a0dd6
ReanGD <reangd@yandex.ru>
2017-04-29 00:59:03 +0300

    boost asio echo app

---
+++ client/main.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+enum { max_length = 1024 };
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    boost::asio::io_service io_service;
+
+    tcp::socket s(io_service);
+    tcp::resolver resolver(io_service);
+    boost::asio::connect(s, resolver.resolve({"127.0.0.1", "5555"}));
+
+    std::string msg = "Hello";
+    boost::asio::write(s, boost::asio::buffer(msg));
+
+    char reply[max_length];
+    size_t reply_length = boost::asio::read(s, boost::asio::buffer(reply, msg.size()));
+    std::cout << "Reply is: ";
+    std::cout.write(reply, reply_length);
+    std::cout << "\n";
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
